7addd2271429d522ca207b99340c50e23387c71c
Arifur Rahman <0arifurrahman0@gmail.com>
2018-07-16 10:36:04 +0600

    Update page-agency.php

--- themes/bhms/page-agency.php
+++ themes/bhms/page-agency.php
@@ -69,11 +69,348 @@ get_header();
 				</div>
 			</div>
 			<div class="col-md-6">
-				
+				<div class="agent-table">
+					<div class="agent-table-title">
+						কর্মকর্তাগনের তথ্য
+					</div>
+					<div class="agent-table-body">
+						<div class="media">
+							<div class="media-left">
+								<a href="#">
+									<img class="media-object" src="<?php esc_url(bloginfo('template_url')) ?>/img/pilgrims-2.png" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">মাওলানা ইয়াকুব শরাফতী</h4>
+								মিরপুর , ঢাকা <br>
+								সৌদি প্রতিনিধি (মোনাজ্জেম)
+							</div>
+						</div>
+
+						<div class="media">
+							<div class="media-left">
+								<a href="#">
+									<img class="media-object" src="<?php esc_url(bloginfo('template_url')) ?>/img/pilgrims-2.png" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">মোঃ আফছার আলী</h4>
+								মিরপুর , ঢাকা <br>
+								সৌদি প্রতিনিধি (মোনাজ্জেম)
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
+<div class="agency-divider"></div>
+<section class="agency">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="agency-heading">
+					<h1>বাৎসরিক তথ্য 
+						<small style="margin-left: 20px;">
+							<i class="fa fa-calendar-minus-o" aria-hidden="true"></i>
+							হজ ২০১৭ /১৪৩৮ হিজরি 
+							<i class="fa fa-angle-down" aria-hidden="true"></i>
+						</small>
+					</h1>
+					<h2 style="color: #2F80ED">হজ প্যাকেজের তথ্য</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="agent-table">
+					<div class="agent-table-body">
+						<table class="table agent-table-right">
+							<tbody>
+								<tr>
+									<td>
+										<img src="<?php esc_url(bloginfo('template_url')) ?>/img/icon/doc.png" alt="" style="margin-right: 5px">
+										প্যাকেজের নাম: 
+									</td>
+									<td>Package-B</td>
+								</tr>
+								<tr>
+									<td>	
+										<img src="<?php esc_url(bloginfo('template_url')) ?>/img/icon/taka.png" alt="" style="margin-right: 5px">
+										প্যাকেজের মূল্য: 
+									</td>
+									<td>Tk. 333,000.00 </td>
+								</tr>
+								<tr>
+									<td>
+										<img src="<?php esc_url(bloginfo('template_url')) ?>/img/icon/bank.png" alt="" style="margin-right: 5px">
+										ব্যাংকে জমাকৃত মূল্য:
+									</td>
+									<td>Tk. 168,943.00</td>
+								</tr>
+							</tbody>
+						</table>
+						<ul class="agent-package-list">
+							<li>প্যাকেজ: 43 দিন</li>
+							<li>প্যাকেজ: 43 দিন</li>
+							<li> ফ্লাইট যাওয়া : Dhaka থেকে Jeddah ফ্লাইট আসা: Jeddah থেকে Dhaka</li>
+							<li> ফ্লাইট যাওয়া : Dhaka থেকে Jeddah ফ্লাইট আসা: Jeddah থেকে Dhaka</li>
+							<li>মিনা অবস্থান কালীন আহার: অন্তর্ভূক্ত</li>
+							<li>মক্কায় প্রথম আবসনের ধরন: হোটেল</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="agent-table">
+					<div class="agent-table-body">
+						<table class="table agent-table-right">
+							<tbody>
+								<tr>
+									<td>
+										<img src="<?php esc_url(bloginfo('template_url')) ?>/img/icon/doc.png" alt="" style="margin-right: 5px">
+										প্যাকেজের নাম: 
+									</td>
+									<td>Package-B</td>
+								</tr>
+								<tr>
+									<td>	
+										<img src="<?php esc_url(bloginfo('template_url')) ?>/img/icon/taka.png" alt="" style="margin-right: 5px">
+										প্যাকেজের মূল্য: 
+									</td>
+									<td>Tk. 333,000.00 </td>
+								</tr>
+								<tr>
+									<td>
+										<img src="<?php esc_url(bloginfo('template_url')) ?>/img/icon/bank.png" alt="" style="margin-right: 5px">
+										ব্যাংকে জমাকৃত মূল্য:
+									</td>
+									<td>Tk. 168,943.00</td>
+								</tr>
+							</tbody>
+						</table>
+						<ul class="agent-package-list">
+							<li>প্যাকেজ: 43 দিন</li>
+							<li>প্যাকেজ: 43 দিন</li>
+							<li> ফ্লাইট যাওয়া : Dhaka থেকে Jeddah ফ্লাইট আসা: Jeddah থেকে Dhaka</li>
+							<li> ফ্লাইট যাওয়া : Dhaka থেকে Jeddah ফ্লাইট আসা: Jeddah থেকে Dhaka</li>
+							<li>মিনা অবস্থান কালীন আহার: অন্তর্ভূক্ত</li>
+							<li>মক্কায় প্রথম আবসনের ধরন: হোটেল</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+							<li>মক্কার হারাম শরীফ হতে হোটেলের দুরত্ব: 200 মিটার</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="agency-heading">
+					<h2 style="color: #2F80ED">
+						হজযাত্রীদের তথ্য বিন্যাস
+					</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="agent-table">
+					<div class="agent-table-body">
+						<canvas id="myChart" width="400" height="400"></canvas>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="agency-heading">
+					<h2 style="color: #2F80ED">
+						হজ গাইড
+					</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="agent-table">
+					<div class="agent-table-body">
+						<div class="media">
+							<div class="media-left">
+								<a href="#">
+									<img class="media-object" src="<?php esc_url(bloginfo('template_url')) ?>/img/pilgrims-2.png" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">
+									মোঃ আক্তার হোসেন
+								</h4>
+								ঢাকা, কোতয়ালী <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 01915399027 <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 0542227779
+							</div>
+						</div>
+
+						<div class="media">
+							<div class="media-left">
+								<a href="#">
+									<img class="media-object" src="<?php esc_url(bloginfo('template_url')) ?>/img/pilgrims-2.png" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">
+									মোঃ আক্তার হোসেন
+								</h4>
+								ঢাকা, কোতয়ালী <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 01915399027 <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 0542227779
+							</div>
+						</div>
+
+						<div class="media">
+							<div class="media-left">
+								<a href="#">
+									<img class="media-object" src="<?php esc_url(bloginfo('template_url')) ?>/img/pilgrims-2.png" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">
+									মোঃ আক্তার হোসেন
+								</h4>
+								ঢাকা, কোতয়ালী <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 01915399027 <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 0542227779
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="agent-table">
+					<div class="agent-table-body">
+						<div class="media">
+							<div class="media-left">
+								<a href="#">
+									<img class="media-object" src="<?php esc_url(bloginfo('template_url')) ?>/img/pilgrims-2.png" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">
+									মোঃ আক্তার হোসেন
+								</h4>
+								ঢাকা, কোতয়ালী <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 01915399027 <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 0542227779
+							</div>
+						</div>
+
+						<div class="media">
+							<div class="media-left">
+								<a href="#">
+									<img class="media-object" src="<?php esc_url(bloginfo('template_url')) ?>/img/pilgrims-2.png" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">
+									মোঃ আক্তার হোসেন
+								</h4>
+								ঢাকা, কোতয়ালী <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 01915399027 <br>
+								<i class="fa fa-phone" aria-hidden="true"></i> 0542227779
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="comment-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10">
+				<div class="agent-table comment-table">
+					<div class="agent-table-body">
+						<!-- Nav tabs -->
+						<ul class="nav nav-tabs custom-tab" role="tablist">
+							<li role="presentation" class="active"><a href="#comments" aria-controls="comments" role="tab" data-toggle="tab">Comments</a></li>
+							<li role="presentation"><a href="#community" aria-controls="community" role="tab" data-toggle="tab">Community</a></li>
+							<li role="presentation" class="pull-right">
+								<a href="#comment-login" aria-controls="comment-login" role="tab" data-toggle="tab">
+									Login
+									<i class="fa fa-angle-down" aria-hidden="true"></i>
+								</a>
+							</li>
+						</ul>
+
+						<!-- Tab panes -->
+						<div class="tab-content custom-tab-content">
+							<div role="tabpanel" class="tab-pane active" id="comments">
+								<form action="" class="comment-form">
+									<div class="form-group">
+										<label for="recommend" class="recommend">
+											<i class="fa fa-heart-o" aria-hidden="true"></i>
+											Recommend
+										</label>
+										<textarea id="recommend" class="form-control" rows="4" placeholder="Start the disccution. . . ." name=""></textarea>
+									</div>
+									<div class="form-group">
+										<button type="submit" class="btn btn-lg btn-primary">Submit</button>
+									</div>
+								</form>
+							</div>
+							<div role="tabpanel" class="tab-pane" id="community">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quas cumque aliquam consequatur a pariatur nam expedita. Expedita dignissimos, quam facere alias dolores officiis, inventore ducimus mollitia totam quis, veritatis.
+							</div>
+							<div role="tabpanel" class="tab-pane" id="comment-login">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio rem dolorum, debitis illum delectus saepe tempora sit fugiat corrupti laudantium, laborum nesciunt architecto. Sapiente omnis fuga beatae, quasi reprehenderit cupiditate!
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
+<script>
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["চিটাগাং", "ঢাকা ", "বারিশাল ", "সিলেট ", "রংপুর ", "রাজশাহী"],
+        datasets: [{
+            label: 'বিভাগ অনুযায়ী হজযাত্রী',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+});
+</script>
 <?php
 //get_sidebar();
 get_footer();
