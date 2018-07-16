def4946f60066f2ffe4438b62b551258f36e63df
Arifur Rahman <0arifurrahman0@gmail.com>
2018-07-16 23:57:06 +0600

    Form Download Page

---
+++ themes/bhms/page-form.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Template Name: Form Download
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bhmp
+ */
+
+get_header();
+?>
+<section id="form-download-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="form-download">
+					<h2>Form Downloads</h2>
+					<table action="" class="table table-striped form-download-table">
+						<thead>
+							<tr>
+								<th width="10%">SL</th>
+								<th width="10%">Form No.</th>
+								<th width="50%">Form Name</th>
+								<th width="15%">Date</th>
+								<th width="15%">Downloads</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>01</td>
+								<td>মাহরামসহ একইসঙ্গে হজে যাওয়ার জন্য নিবন্ধন ফরম	</td>
+								<td>21 - May - 2018</td>
+								<td>
+									<a href="#">
+										<i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+										Download PDF
+									</a>
+								</td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>01</td>
+								<td>হজযাত্রীদের তালিকাসহ গাইড প্রস্তাবের ফরম (অনলাইন সিস্টেমে তথ্য পূরণ করে প্রিন্ট আউট নিতে হবে। সংযুক্ত ফরমটি শুধুমাত্র ধারণার জন্য দেওয়া হলো)।</td>
+								<td>21 - May - 2018</td>
+								<td>
+									<a href="#">
+										<i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+										Download PDF
+									</a>
+								</td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>01</td>
+								<td>জাতীয় হজ ও ওমরাহ নীতির ৩.১.১৭ অনুচ্ছেদের আলোকে হজযাত্রী পরিবর্তনের ফরম (অনলাইন সিস্টেমে তথ্য পূরণ করে প্রিন্ট আউট নিতে হবে। সংযুক্ত ফরমটি শুধুমাত্র ধারণার জন্য দেওয়া হলো)।	</td>
+								<td>21 - May - 2018</td>
+								<td>
+									<a href="#">
+										<i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+										Download PDF
+									</a>
+								</td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>01</td>
+								<td>প্যাকেজ ভিত্তিক গাইডসহ হজযাত্রীদের তালিকা (অনলাইন সিস্টেমে তথ্য পূরণ করে প্রিন্ট আউট নিতে হবে। সংযুক্ত ফরমটি শুধুমাত্র ধারণার জন্য দেওয়া হলো)।	</td>
+								<td>21 - May - 2018</td>
+								<td>
+									<a href="#">
+										<i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+										Download PDF
+									</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php
+//get_sidebar();
+get_footer();
+?>
